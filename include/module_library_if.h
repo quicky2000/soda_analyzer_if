4416b949e4fa47cb00025d50299b336f03ee0eee
Julien Thevenon <julien_thevenon@yahoo.fr>
2012-11-13 21:43:48 +0100

    Modify code to compile in ansi pedantic mode

--- include/module_library_if.h
+++ include/module_library_if.h
@@ -21,7 +21,7 @@
 
 #include "general_analyzer_if.h"
 #include "module_configuration.h"
-#include <stdint.h>
+#include <inttypes.h>
 
 namespace osm_diff_analyzer_if
 {
@@ -32,7 +32,7 @@ namespace osm_diff_analyzer_if
   {
   public:
 
-    typedef void(*t_register_function)(void **,uint32_t);
+    typedef void(*t_register_function)(uintptr_t *,uint32_t);
 
     typedef general_analyzer_if*(*t_create_analyzer)(const module_configuration * p_conf);
     typedef analyzer_description_if * (*t_get_description)(void);
